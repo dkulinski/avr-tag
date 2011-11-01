17a0a39ad4d6b1ee042ed452bea80f33c3120fca
Dan <dan@prey.(none)>
2011-10-31 23:01:10 -0600

    Changed to timing counter interrupt polling, works a treat!

--- main.c
+++ main.c
@@ -7,9 +7,12 @@
 uint8_t led = 0x00;
 
 // Interrupt to handle changes on pin 14 (PCINT0)
-ISR(PCINT0_vect)
+ISR(TIMER0_COMPA_vect)
 {
-	led = 0xff;
+	if( ~PINB & _BV(PB0) )
+	{
+		led = 0x2f;
+	}
 }
 
 int main() 
@@ -18,22 +21,29 @@ int main()
   DDRD=0b10000000;
 	// Operational LED pin set to output
 	DDRB=0b00100000;
+	DDRB &= ~_BV(DDB0);
+
 	// Turn on power to operation LED and set pull-up resistor for IR signal in
 	PORTB=0b00100001;
 
 	// Start status LEDs in off mode
 	PORTD=0b10000000;
 
-	// Pin Change Interrup Control Register - Enable the lowest Pin Change Register
-	PCICR = _BV(PCIE0);
-	// Pint Change Mask 0 - Enable PCINT0
-	PCMSK0 = _BV(PCINT0);
+	// Enable timer 0 with 8 bit prescaler
+	// Count for 26 microseconds
+	TCCR0A = _BV(WGM00) | _BV(WGM01);
+	TCCR0B = _BV(WGM02) | _BV(CS01);
+	OCR0A = 52;
+
+	// Enable Output compare match
+	TIMSK0 = _BV(OCIE0A);
 
 	// Enable interrupts according to registers
 	sei();
 
   while(1)
   {
+
 		if(led > 0)
 		{
 			led--;
