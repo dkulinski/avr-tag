3ff7b82101ea04593e6807311c1314f7c989a462
Dan Kulinski <dan@dan-test.(none)>
2011-11-20 17:12:48 -0700

    Added game event enum

--- main.c
+++ main.c
@@ -10,6 +10,19 @@
 #define HIT_PIN 128   // Hit LEDs are driven from PD7
 #define BUZZER_PIN 8  // Piezo buzzer driven on PD3
 
+// States for the game
+// 
+// The game can be running, over,waiting to start,
+// or in game settings
+//
+// When the game is running check IR sensor, fire, play appropriate
+// sounds.  
+// When the game is in the wait state, player one will pull trigger
+// to start the game
+// In the settings menu (to be done) the player can change settings
+// of the gun
+enum states { GAME_RUNNING, GAME_OVER, GAME_WAIT, GAME_SETTINGS };
+
 int main()
 {
 	return 0;
